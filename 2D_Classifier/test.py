0d317024e15e95aa5ff64f3694ed8a03852266c8
joaonogueiro <joao.nogueiro@ua.pt>
2024-02-07 09:00:49 +0000

    Final commit

--- 2D_Classifier/test.py
+++ 2D_Classifier/test.py
@@ -9,6 +9,7 @@ import torch.nn.functional as F
 from dataset import Dataset
 from model import Model
 from sklearn import metrics
+from torchvision import transforms
 
 def main():
     # -----------------------------------------------------------------
@@ -22,6 +23,10 @@ def main():
     with open('dataset_filenames.json', 'r') as f:
         # Reading from json file
         dataset_filenames = json.load(f)
+           
+    with open('categories.json', 'r') as f:
+        categories_dict = json.load(f)
+
 
     test_filenames = dataset_filenames['test_filenames']
     print(f'Test {len(test_filenames)} images')
@@ -47,6 +52,9 @@ def main():
     model.to(device)
     model.eval()  # we are in testing mode
 
+    # Just for testing the train_loader
+    tensor_to_pil_image = transforms.ToPILImage()
+
     predicted_is = []
     labels_gt_np = []
 
@@ -70,10 +78,49 @@ def main():
 
     # print(labels_gt_np)
     # print(predicted_is)
+    
+    # Show Images
+        
+    labels_names = categories_dict
+
+    labels_names = {value: key for key, value in labels_names.items()}
+    fig = plt.figure()
+    idx_image = 0
+    for row in range(6):
+        for cols in range(6):
+            image_tensor = inputs[idx_image, :, :, :]
+            image_pil = tensor_to_pil_image(image_tensor)
+
+            ax = fig.add_subplot(6,6,idx_image+1)
+            plt.imshow(image_pil)
+
+            # Remove axis
+            ax.xaxis.set_ticklabels([])
+            ax.yaxis.set_ticklabels([])
+            ax.xaxis.set_ticks([])
+            ax.yaxis.set_ticks([])
+
+            ground_truth = labels_gt_np[idx_image]
+            predicted = predicted_is[idx_image]
+
+            if ground_truth == predicted:
+                color = 'green'
+            else:
+                color = 'red'
+
+            text = labels_names.get(predicted)
+            # Put labels
+            ax.set_xlabel(text, color = color)
+
+            idx_image +=1
+
+    plt.show()
+    
         
     Precision = metrics.precision_score(labels_gt_np, predicted_is, average='weighted')
     Sensitivity_recall = metrics.recall_score(labels_gt_np, predicted_is, average='weighted')
     F1_score = metrics.f1_score(labels_gt_np, predicted_is, average='weighted')
+    print('\nGeneral metrics of the 2D classifier:')
     print(f'Precision = {Precision:.2f}') # In the simplest terms, Precision is the ratio between the True Positives and all the points that are classified as Positives.
     print(f'Recall = {Sensitivity_recall:.2f}') # To put it simply, Recall is the measure of our model correctly identifying True Positives. It is also called a True positive rate.
     print(f'F1_score = {F1_score:.2f}') # F1-score is the Harmonic mean of the Precision and Recall
@@ -92,6 +139,7 @@ def main():
     cm_display.plot(cmap='viridis', values_format='.2f')
 
     plt.show()
+
 
 if __name__ == "__main__":
     main()
